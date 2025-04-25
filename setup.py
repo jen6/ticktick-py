94ee68ed68fb320bd1cf20fc7496ef06a75af161
jen6 <work.jen6@gmail.com>
2025-04-25 15:50:20 +0900

    bump version to 2.0.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/lazeroffmichael/ticktick-py'
 EMAIL = 'lazeroffmichael@gmail.com'
 AUTHOR = 'Michael Lazeroff'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '2.0.3'
+VERSION = '2.0.4'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
